8ebd924722ca58172b5f09f4ab3e0efb57b9d342
Doctor_Mod <davidgolding05@gmail.com>
2019-07-17 19:01:32 +0800

    I may have kinda gone overboard and have done like pretty much all of it

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 function sendmsg(input) {
-  console.log(input)
+  CreateMessage(document.getElementById('input').value);
+  document.getElementById('input').value = "";
+}
+
+document.getElementById('sendbox').addEventListener("submit", sendmsg);
+
+function CreateMessage(message) {
+
+  var icon = document.createElement("i");
+  icon.classList.add("fas", "fa-user", "Left", "Icon");
+  var bubble = document.createElement("div");
+  bubble.classList.add("Bubble", "White");
+  bubble.innerHTML = '<p class="you">' + message + '</p>';
+  var divider = document.createElement("div");
+  divider.classList.add("divider");
+  document.getElementById('main').appendChild(icon);
+  document.getElementById('main').appendChild(bubble);
+  document.getElementById('main').appendChild(divider);
 }
