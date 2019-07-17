612b81f7c529b90dc1e167aadbda8632e8972c2a
Doctor_Mod <davidgolding05@gmail.com>
2019-07-17 20:08:50 +0800

    Push

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 function sendmsg(input) {
   CreateMessage(document.getElementById('input').value, "you");
+  localStorage.send = document.getElementById('input').value;
+  latest = document.getElementById('input').value;
   document.getElementById('input').value = "";
 }
+
+localStorage.send = '';
+var latest = '';
 
 document.getElementById('sendbox').addEventListener("submit", sendmsg);
 
@@ -32,4 +37,14 @@ function CreateMessage(message, request) {
   } else {
     console.error("request is set to an incorrect value.");
   }
+}
+
+repeat();
+
+function repeat() {
+  if (latest != localStorage.send) {
+    CreateMessage(localStorage.send, "them");
+    latest = localStorage.send;
+  }
+  setTimeout(repeat, 100);
 }
