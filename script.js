76f2affe0df8517ffd7e0f112f5cd7f8960098de
Doctor_Mod <davidgolding05@gmail.com>
2019-07-17 19:14:50 +0800

    edited create message js

--- script.js
+++ script.js
@@ -1,20 +1,35 @@
 function sendmsg(input) {
-  CreateMessage(document.getElementById('input').value);
+  CreateMessage(document.getElementById('input').value, "you");
   document.getElementById('input').value = "";
 }
 
 document.getElementById('sendbox').addEventListener("submit", sendmsg);
 
-function CreateMessage(message) {
+function CreateMessage(message, request) {
 
-  var icon = document.createElement("i");
-  icon.classList.add("fas", "fa-user", "Left", "Icon");
-  var bubble = document.createElement("div");
-  bubble.classList.add("Bubble", "White");
-  bubble.innerHTML = '<p class="you">' + message + '</p>';
-  var divider = document.createElement("div");
-  divider.classList.add("divider");
-  document.getElementById('main').appendChild(icon);
-  document.getElementById('main').appendChild(bubble);
-  document.getElementById('main').appendChild(divider);
+  if (request == "you") {
+    var icon = document.createElement("i");
+    icon.classList.add("fas", "fa-user", "Left", "Icon");
+    var bubble = document.createElement("div");
+    bubble.classList.add("Bubble", "White");
+    bubble.innerHTML = '<p class="you">' + message + '</p>';
+    var divider = document.createElement("div");
+    divider.classList.add("divider");
+    document.getElementById('main').appendChild(icon);
+    document.getElementById('main').appendChild(bubble);
+    document.getElementById('main').appendChild(divider);
+  } else if (request == "them") {
+    var icon = document.createElement("i");
+    icon.classList.add("fas", "fa-user", "Right", "Icon");
+    var bubble = document.createElement("div");
+    bubble.classList.add("Bubble", "Blue");
+    bubble.innerHTML = '<p class="them">' + message + '</p>';
+    var divider = document.createElement("div");
+    divider.classList.add("divider");
+    document.getElementById('main').appendChild(icon);
+    document.getElementById('main').appendChild(bubble);
+    document.getElementById('main').appendChild(divider);
+  } else {
+    console.error("request is set to an incorrect value.");
+  }
 }
