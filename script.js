66337ae86b086dacefbd16e7e457463624ee93b1
DoctorMod <46367601+DoctorMod@users.noreply.github.com>
2019-07-23 17:25:11 +0800

    Did everything

--- script.js
+++ script.js
@@ -1,35 +1,57 @@
-function sendmsg(input) {
-  CreateMessage(document.getElementById('input').value, "you");
-  document.getElementById('input').value = "";
-}
+$(document).ready(function () {
+  // Initialize the PubNub API connection.
+  var pubnub = PUBNUB.init({
+    publish_key: 'pub-c-b6140a7a-583f-4085-aaa6-49fe56da1533',
+    subscribe_key: 'sub-c-e38a37dc-ad26-11e9-a87a-b2acb6d6da6e'
+  });
+ 
+  // Grab references for all of our elements.
+  var messageContent = $('#messageContent'),
+      sendMessageButton = $('#sendMessageButton'),
+      messageList = $('#messageList');
+ 
+  // Handles all the messages coming in from pubnub.subscribe.
+  function handleMessage(message) {
+    var messageEl = $("<li class='message'>"
+        + "<span class='username'>" + message.username + ": </span>"
+        + message.text
+        + "</li>");
+    messageList.append(messageEl);
+ 
+    // Scroll to bottom of page
+    $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 'slow');
+  };
 
-document.getElementById('sendbox').addEventListener("submit", sendmsg);
+  // Compose and send a message when the user clicks our send message button.
+  sendMessageButton.click(function (event) {
+    var message = messageContent.val();
+ 
+    if (message != '') {
+      pubnub.publish({
+        channel: 'chat',
+        message: {
+          username: $('#Username').val(),
+          text: message
+        }
+      });
+ 
+      messageContent.val("");
+    }
+	localStorage.username = $('#Username').val();
+  });
+ 
+  // Also send a message when the user hits the enter button in the text area.
+  messageContent.bind('keydown', function (event) {
+    if((event.keyCode || event.charCode) !== 13) return true;
+    sendMessageButton.click();
+    return false;
+  });
+ 
+  // Subscribe to messages coming in from the channel.
+  pubnub.subscribe({
+    channel: 'chat',
+    message: handleMessage
+  });
+});
 
-function CreateMessage(message, request) {
-
-  if (request == "you") {
-    var icon = document.createElement("i");
-    icon.classList.add("fas", "fa-user", "Left", "Icon");
-    var bubble = document.createElement("div");
-    bubble.classList.add("Bubble", "White");
-    bubble.innerHTML = '<p class="you">' + message + '</p>';
-    var divider = document.createElement("div");
-    divider.classList.add("divider");
-    document.getElementById('main').appendChild(icon);
-    document.getElementById('main').appendChild(bubble);
-    document.getElementById('main').appendChild(divider);
-  } else if (request == "them") {
-    var icon = document.createElement("i");
-    icon.classList.add("fas", "fa-user", "Right", "Icon");
-    var bubble = document.createElement("div");
-    bubble.classList.add("Bubble", "Blue");
-    bubble.innerHTML = '<p class="them">' + message + '</p>';
-    var divider = document.createElement("div");
-    divider.classList.add("divider");
-    document.getElementById('main').appendChild(icon);
-    document.getElementById('main').appendChild(bubble);
-    document.getElementById('main').appendChild(divider);
-  } else {
-    console.error("request is set to an incorrect value.");
-  }
-}
+document.getElementById('Username').value = localStorage.username;
