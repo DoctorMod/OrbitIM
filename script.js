0d5c3de8a7562b21a7e3eaabc92c0f35a74517ac
Doctor_Mod <davidgolding05@gmail.com>
2019-07-25 15:58:56 +0800

    Push Script

--- script.js
+++ script.js
@@ -1,42 +1,59 @@
 var pubnub;
-$(document).ready(function () {
+$(document).ready(function() {
   // Initialize the PubNub API connection.
   pubnub = PUBNUB.init({
     publish_key: 'pub-c-b6140a7a-583f-4085-aaa6-49fe56da1533',
     subscribe_key: 'sub-c-e38a37dc-ad26-11e9-a87a-b2acb6d6da6e'
   });
-	
+
   // Grab references for all of our elements.
   var messageContent = $('#messageContent'),
-      sendMessageButton = $('#sendMessageButton'),
-      messageList = $('#messageList');
-	var users = [];
+    sendMessageButton = $('#sendMessageButton'),
+    messageList = $('#messageList');
+  var users = [];
   // Handles all the messages coming in from pubnub.subscribe.
   function handleMessage(message) {
-	  var classVal = -1;
-		  for (var i = 0; i < users.length; i++) {
-			  if (users[i] == message.username) {
-				  classVal = i + 1;
-			  }
-			}
-			
-			if (classVal == -1) {
-				users.push(message.username);
-				classVal = users.length;
-			}
-    var messageEl = $("<li class='message "+ classVal +"'>"
-        + "<span class='username'>" + message.username + ": </span>"
-        + message.text
-        + "</li>");
+    var classVal = -1;
+    for (var i = 0; i < users.length; i++) {
+      if (users[i] == message.username) {
+        classVal = i + 1;
+      }
+    }
+
+    if (classVal == -1) {
+      users.push(message.username);
+      classVal = users.length;
+    }
+    var messageEl = $("<li class='message " + classVal + "'>" +
+      "<span class='username'>" + message.username + ": </span>" +
+      message.text +
+      "</li>");
     messageList.append(messageEl);
     // Scroll to bottom of page
-    $("html, body").animate({ scrollTop: $(document).height() - $(window).height() }, 'slow');
+    $("html, body").animate({
+      scrollTop: $(document).height() - $(window).height()
+    }, 'slow');
+  };
+
+  function handleMembers(message) {
+    if (message.send == localStorage.username) {
+      var memberEl = $("<li class='member'>" + message.username + "</li>");
+      memberList.append(memberEl);
+    } else {
+      pubnub.publish({
+        channel: 'members',
+        message: {
+          username: $('#Username').val(),
+          send: message.send
+        }
+      });
+    }
   };
 
   // Compose and send a message when the user clicks our send message button.
-  sendMessageButton.click(function (event) {
+  sendMessageButton.click(function(event) {
     var message = messageContent.val();
- 
+
     if (message != '') {
       pubnub.publish({
         channel: 'chat',
@@ -45,48 +62,61 @@ $(document).ready(function () {
           text: message
         }
       });
- 
+
       messageContent.val("");
     }
-	localStorage.username = $('#Username').val();
+    localStorage.username = $('#Username').val();
   });
- 
+
   // Also send a message when the user hits the enter button in the text area.
-  messageContent.bind('keydown', function (event) {
-    if((event.keyCode || event.charCode) !== 13) return true;
+  messageContent.bind('keydown', function(event) {
+    if ((event.keyCode || event.charCode) !== 13) return true;
     sendMessageButton.click();
     return false;
   });
- 
+
   // Subscribe to messages coming in from the channel.
   pubnub.subscribe({
     channel: 'chat',
     message: handleMessage
   });
-  
+  pubnub.subscribe({
+    channel: 'members',
+    message: handleMembers
+  });
+
   pubnub.publish({
-        channel: 'chat',
-        message: {
-          username: $('#Username').val(),
-			text: "has joined the channel"
-        }
-      });
+    channel: 'chat',
+    message: {
+      username: $('#Username').val(),
+      text: "has joined the channel"
+    }
+  });
+
+  pubnub.publish({
+    channel: 'members',
+    message: {
+      username: $('#Username').val(),
+      send: localStorage.username
+    }
+  });
 });
-if(localStorage.username == undefined) {
-		localStorage.username = "user"+Math.floor(Math.random()*100);
-	}
+if (localStorage.username == undefined) {
+  localStorage.username = "user" + Math.floor(Math.random() * 100);
+}
 document.getElementById('Username').value = localStorage.username;
 document.getElementById('logonUser').innerText = localStorage.username;
+
 function reset() {
-	document.getElementById('messageList').innerHTML = '';
+  document.getElementById('messageList').innerHTML = '';
 }
 
-window.onbeforeunload = function(){	 
-	 pubnub.publish({
-        channel: 'chat',
-        message: {
-          username: $('#Username').val(),
-			text: "has left the channel"
-        }
-      });
+window.onbeforeunload = function() {
+  pubnub.publish({
+    channel: 'chat',
+    message: {
+      username: $('#Username').val(),
+      text: "has left the channel"
+    }
+  });
 }
