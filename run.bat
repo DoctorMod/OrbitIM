08745000e4434890c53d6a00dac0d7c55c584d11
DoctorMod <davidgolding05@gmail.com>
2021-04-08 15:07:33 +0800

    Added Port

--- run.bat
+++ run.bat
@@ -1,5 +1,5 @@
 @echo off
 for /f "delims=[] tokens=2" %%a in ('ping -4 -n 1 %ComputerName% ^| findstr [') do set NetworkIP=%%a
-echo Network IP: %NetworkIP%
+echo Network IP: %NetworkIP%:3000
 node index.js > out.log
 PAUSE
